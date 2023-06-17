ab91d4dccfb5ad6d1c6cf4d33dcc593ead377dd2
tertius Jubber <tjubber@gmail.com>
2023-06-17 19:17:18 +0200

    df

--- code.js
+++ code.js
@@ -1,14 +1,1 @@
-const switchItUp = (number) =>
-          number == 0 ?'Zero':
-          number == 1 ?'One':
-          number == 2 ?'Two':
-          number == 3 ?'Three':
-          number == 4 ?'Four':
-          number == 5 ?'Five':
-          number == 6 ?'Six':
-          number == 7 ?'Seven':
-          number == 8 ?'Eight':
-          number == 9 ?'Nine':'hi'
-
-
-console.log(switchItUp(1));
+const squareDigits = num => num.map(num.toString()).
